d922d6150f760a5fbfc10eaa487b2a9e0cb5d024
Richard Forjoe <richard.forjoe@sky.uk>
2023-05-14 01:46:59 +0100

    feat(spark): added a tool version of the command

---
+++ slack-message-tool/slackServiceRequest.js
@@ -0,0 +1,23 @@
+const { WebClient } = require('@slack/web-api');
+require('dotenv').config();
+
+const token = process.env.SLACK_TOKEN_PERSONAL; // You should replace this with your actual token
+
+const web = new WebClient(token);
+
+const sendMessageConfig = (data) => {
+  return {
+    channel: data.channel,
+    text: data.text,
+    blocks: data.blocks,
+  };
+};
+
+exports.sendSlackMessage = async (data) => {
+  console.log("DATA", data)
+  try {
+    await web.chat.postMessage(sendMessageConfig(data));
+  } catch (error) {
+    console.error(error);
+  }
+};
